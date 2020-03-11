ad84fb78df78960ea444c75640bca4afa1668d5b
Arturo Araujo <arturo.araujo001@outlook.com>
2020-03-11 01:06:13 -0400

    n

---
+++ sendmail.php
@@ -0,0 +1,6 @@
+<?php
+
+    $site_owners_email = 'arturo.araujo001@outlook.com'; // Replace this with your own email address
+    $site_owners_name = 'Arturo Araujo'; // replace with your name
+
+?>
